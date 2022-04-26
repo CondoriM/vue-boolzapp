083277c16b67e977590719205b3764642b1df113
CondoriM <condoria.miguel@gmail.com>
2022-04-26 16:12:33 +0200

    completed basic layout

--- assets/app.js
+++ assets/app.js
@@ -27,7 +27,7 @@ const app = new Vue({
             },
             {
                 name: 'Fabio',
-                avatar: '_2',
+                avatar: './assets/img/avatar_2.jpg',
                 visible: true,
                 messages: [
                     {
@@ -49,7 +49,7 @@ const app = new Vue({
             },
             {
                 name: 'Samuele',
-                avatar: '_3',
+                avatar: './assets/img/avatar_3.jpg',
                 visible: true,
                 messages: [
                     {
@@ -71,7 +71,7 @@ const app = new Vue({
             },
             {
                 name: 'Alessandro B.',
-                avatar: '_4',
+                avatar: './assets/img/avatar_4.jpg',
                 visible: true,
                 messages: [
                     {
@@ -88,7 +88,7 @@ const app = new Vue({
             },
             {
                 name: 'Alessandro L.',
-                avatar: '_5',
+                avatar: './assets/img/avatar_5.jpg',
                 visible: true,
                 messages: [
                     {
@@ -105,7 +105,7 @@ const app = new Vue({
             },
             {
                 name: 'Claudia',
-                avatar: '_6',
+                avatar: './assets/img/avatar_6.jpg',
                 visible: true,
                 messages: [
                     {
@@ -127,7 +127,7 @@ const app = new Vue({
             },
             {
                 name: 'Federico',
-                avatar: '_7',
+                avatar: './assets/img/avatar_7.jpg',
                 visible: true,
                 messages: [
                     {
@@ -144,7 +144,7 @@ const app = new Vue({
             },
             {
                 name: 'Davide',
-                avatar: '_8',
+                avatar: './assets/img/avatar_8.jpg',
                 visible: true,
                 messages: [
                     {
